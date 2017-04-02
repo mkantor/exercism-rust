9c19ced6d4cf776ee3d89669e609037b6b3bfec9
Matt Kantor <the.matt.kantor@gmail.com>
2017-04-02 16:08:34 -0700

    Overkill solution for sieve.
    I wanted to play with Rust's tools for abstraction some more.

--- sieve/src/lib.rs
+++ sieve/src/lib.rs
@@ -1,25 +1,75 @@
-use std::collections::HashSet;
+use sieve::SieveStorage;
 
-pub fn primes_up_to(limit: usize) -> Vec<usize> {
-    let mut non_primes = HashSet::new();
-    for num in 2..(limit / 2) {
-        if non_primes.contains(&num) {
-            continue;
-        } else {
-            let mut multiple = num;
-            while multiple + num <= limit {
-                multiple += num;
-                non_primes.insert(multiple);
+mod sieve {
+    use std::ops::Range;
+
+    pub struct SieveStorage {
+        entries: Vec<bool>,
+        start: usize,
+    }
+
+    pub struct Iter<'a> {
+        data: &'a SieveStorage,
+        current: usize,
+    }
+
+    impl SieveStorage {
+        pub fn new(range: Range<usize>) -> Self {
+            SieveStorage {
+                entries: vec![true; range.len()],
+                start: range.start,
+            }
+        }
+
+        pub fn remove(&mut self, num: usize) {
+            let index = num - self.start;
+            if let Some(entry) = self.entries.get_mut(index) {
+                *entry = false;
+            }
+        }
+
+        pub fn contains(&self, num: usize) -> bool {
+            let index = num - self.start;
+            *self.entries.get(index).unwrap_or(&false)
+        }
+
+        pub fn iter(&self) -> Iter {
+            Iter {
+                current: self.start,
+                data: self,
             }
         }
     }
 
-    let mut primes = vec![];
-    for num in 2..(limit + 1) {
-        if !non_primes.contains(&num) {
-            primes.push(num);
+    impl<'a> Iterator for Iter<'a> {
+        type Item = usize;
+
+        fn next(&mut self) -> Option<usize> {
+            while self.current < self.data.entries.len() + self.data.start {
+                self.current += 1;
+                if self.data.contains(self.current - 1) {
+                    return Some(self.current - 1);
+                }
+            }
+            None
+        }
+    }
+}
+
+pub fn primes_up_to(limit: usize) -> Vec<usize> {
+    let mut sieve = SieveStorage::new(2..(limit + 1));
+
+    for num in 2..(limit / 2) {
+        if !sieve.contains(num) {
+            continue;
+        } else {
+            let mut multiple = num * 2;
+            while multiple <= limit {
+                sieve.remove(multiple);
+                multiple += num;
+            }
         }
     }
 
-    primes
+    sieve.iter().collect()
 }
