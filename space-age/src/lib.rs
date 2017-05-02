9dd0caa3dca688183f2ea11ed6e2cfedf90b0708
Matt Kantor <mkantor@kabam.com>
2017-05-02 16:15:58 -0700

    Implement space-age.

--- space-age/src/lib.rs
+++ space-age/src/lib.rs
@@ -1,18 +1,20 @@
-// The code below is a stub. Just enough to satisfy the compiler.
-// In order to pass the tests you can add-to or change any of this code.
-#![allow(unused_variables)]
-
-pub struct Duration;
+pub struct Duration {
+    seconds: u64,
+}
 
 impl From<u64> for Duration {
     fn from(s: u64) -> Self {
-        unimplemented!()
+        Duration { seconds: s }
     }
 }
 
 pub trait Planet {
+    // Associated constants aren't stabilized yet, otherwise I'd prefer this:
+    // const YEAR_LEN: Duration;
+    fn year_len() -> Duration;
+
     fn years_during(d: &Duration) -> f64 {
-        unimplemented!();
+        d.seconds as f64 / Self::year_len().seconds as f64
     }
 }
 
@@ -25,11 +27,43 @@ pub struct Saturn;
 pub struct Uranus;
 pub struct Neptune;
 
-impl Planet for Mercury {}
-impl Planet for Venus {}
-impl Planet for Earth {}
-impl Planet for Mars {}
-impl Planet for Jupiter {}
-impl Planet for Saturn {}
-impl Planet for Uranus {}
-impl Planet for Neptune {}
+impl Planet for Mercury {
+    fn year_len() -> Duration {
+        Duration::from(7600544)
+    }
+}
+impl Planet for Venus {
+    fn year_len() -> Duration {
+        Duration::from(19414149)
+    }
+}
+impl Planet for Earth {
+    fn year_len() -> Duration {
+        Duration::from(31557600)
+    }
+}
+impl Planet for Mars {
+    fn year_len() -> Duration {
+        Duration::from(59354033)
+    }
+}
+impl Planet for Jupiter {
+    fn year_len() -> Duration {
+        Duration::from(374355659)
+    }
+}
+impl Planet for Saturn {
+    fn year_len() -> Duration {
+        Duration::from(929292363)
+    }
+}
+impl Planet for Uranus {
+    fn year_len() -> Duration {
+        Duration::from(2651370019)
+    }
+}
+impl Planet for Neptune {
+    fn year_len() -> Duration {
+        Duration::from(5200418560)
+    }
+}
