f62adacd62e166b707c126747ae96238a7d9a53f
Matt Kantor <mkantor@kabam.com>
2017-05-02 11:18:47 -0700

    Minor performance/readability improvement for sublist solution.

--- sublist/src/lib.rs
+++ sublist/src/lib.rs
@@ -12,7 +12,7 @@ pub fn sublist<A>(list_1: &[A], list_2: &[A]) -> Comparison
     match (list_1.len(), list_2.len()) {
         (len_1, len_2) if len_1 < len_2 && is_sublist(list_1, list_2) => Comparison::Sublist,
         (len_1, len_2) if len_2 < len_1 && is_sublist(list_2, list_1) => Comparison::Superlist,
-        (len_1, len_2) if len_1 == len_2 && is_sublist(list_1, list_2) => Comparison::Equal,
+        _ if list_1 == list_2 => Comparison::Equal,
         _ => Comparison::Unequal,
     }
 }
