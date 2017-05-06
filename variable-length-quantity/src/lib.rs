aed2482afc392258788810a11b3725934f8f6e01
Matt Kantor <mkantor@kabam.com>
2017-05-06 13:50:29 -0700

    Improve readability of variable-length-quantity.

--- variable-length-quantity/src/lib.rs
+++ variable-length-quantity/src/lib.rs
@@ -4,15 +4,15 @@ pub enum Error {
     OverflowDuringDecoding,
 }
 
-struct VLQBytes {
+struct VLQEncoder {
     number: u32,
     remaining_bytes: u8,
 }
 
-impl VLQBytes {
-    fn new(number: &u32) -> VLQBytes {
+impl VLQEncoder {
+    fn new(number: &u32) -> Self {
         let significant_bits = 32 - number.leading_zeros() as u8;
-        VLQBytes {
+        Self {
             number: *number,
             remaining_bytes: if *number == 0 {
                 1
@@ -25,37 +25,37 @@ impl VLQBytes {
     }
 }
 
-impl Iterator for VLQBytes {
+impl Iterator for VLQEncoder {
     type Item = u8;
 
     fn next(&mut self) -> Option<Self::Item> {
         if self.remaining_bytes > 1 {
-            let bits = (self.number >> (self.remaining_bytes - 1) * 7) & 0b01111111;
             self.remaining_bytes -= 1;
-            Some((bits | 0b10000000) as u8)
+            let bits = (self.number >> (self.remaining_bytes * 7)) & 0b0111_1111;
+            Some((bits | 0b1000_0000) as u8)
         } else if self.remaining_bytes == 1 {
             self.remaining_bytes -= 1;
-            Some((self.number & 0b01111111) as u8)
+            Some((self.number & 0b0111_1111) as u8)
         } else {
             None
         }
     }
 }
 
-fn vlq_has_more_bytes(byte: &u8) -> bool {
-    (byte & 0b10000000) == 0b10000000
+fn is_last_byte(byte: &u8) -> bool {
+    (byte & 0b1000_0000) == 0
 }
 
 /// Convert a list of numbers to a stream of bytes encoded with variable length encoding.
 pub fn to_bytes(values: &[u32]) -> Vec<u8> {
-    values.iter().flat_map(VLQBytes::new).collect()
+    values.iter().flat_map(VLQEncoder::new).collect()
 }
 
 /// Given a stream of bytes, extract all numbers which are encoded in there.
 pub fn from_bytes(bytes: &[u8]) -> Result<Vec<u32>, Error> {
     match bytes.last() {
         None => Ok(vec![]),
-        Some(last_byte) if vlq_has_more_bytes(last_byte) => Err(Error::EncodedNumberIncomplete),
+        Some(last_byte) if !is_last_byte(last_byte) => Err(Error::EncodedNumberIncomplete),
         Some(_) => {
             let mut values = vec![];
             let mut current_value = 0u32;
@@ -67,10 +67,8 @@ pub fn from_bytes(bytes: &[u8]) -> Result<Vec<u32>, Error> {
                     current_value <<= 7;
                 }
 
-                if vlq_has_more_bytes(byte) {
-                    current_value += (byte ^ 0b10000000) as u32;
-                } else {
-                    current_value += *byte as u32;
+                current_value |= (byte & 0b0111_1111) as u32;
+                if is_last_byte(byte) {
                     values.push(current_value);
                     current_value = 0;
                 }
