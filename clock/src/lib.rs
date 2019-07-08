1e4a63cfa832a709de00140f8c918352566fa164
Matt Kantor <the.matt.kantor@gmail.com>
2019-07-07 19:07:29 -0700

    Implement clock.

--- clock/src/lib.rs
+++ clock/src/lib.rs
@@ -1,15 +1,31 @@
-pub struct Clock;
+#[derive(Debug, PartialEq)]
+pub struct Clock {
+    minute_of_day: u16
+}
 
 impl Clock {
-    pub fn new(hours: i32, minutes: i32) -> Self {
-        unimplemented!(
-            "Construct a new Clock from {} hours and {} minutes",
-            hours,
-            minutes
-        );
+    pub fn new(hours: isize, minutes: isize) -> Self {
+        let total_minutes = hours % 24 * 60 + minutes;
+        if total_minutes < 0 {
+            Self {
+                minute_of_day: (1440 + total_minutes % 1440) as u16
+            }
+        } else {
+            Self {
+                minute_of_day: (total_minutes % 1440) as u16
+            }
+        }
     }
 
-    pub fn add_minutes(&self, minutes: i32) -> Self {
-        unimplemented!("Add {} minutes to existing Clock time", minutes);
+    pub fn add_minutes(&self, minutes: isize) -> Self {
+        Self::new(0, self.minute_of_day as isize + minutes)
+    }
+}
+
+impl std::fmt::Display for Clock {
+    fn fmt(&self, formatter: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let hours = self.minute_of_day / 60;
+        let minutes = self.minute_of_day - hours * 60;
+        write!(formatter, "{:02}:{:02}", hours, minutes)
     }
 }
