1d1da8ca533bcaa14180792dbfabd5ee01b470bd
Matt Kantor <the.matt.kantor@gmail.com>
2019-08-17 16:13:55 -0700

    Implement collatz-conjecture.

--- collatz-conjecture/src/lib.rs
+++ collatz-conjecture/src/lib.rs
@@ -1,6 +1,17 @@
 pub fn collatz(n: u64) -> Option<u64> {
-    unimplemented!(
-        "return Some(x) where x is the number of steps required to reach 1 starting with {}",
-        n,
-    )
+    if n <= 0 {
+        None
+    } else {
+        let mut current_n = n;
+        let mut steps = 0;
+        while current_n != 1 {
+            if current_n % 2 == 0 {
+                current_n = current_n / 2;
+            } else {
+                current_n = current_n * 3 + 1;
+            }
+            steps += 1;
+        }
+        Some(steps)
+    }
 }
