9d71b65c1a845e9f7e5679a46c24b2e41687bfe0
Matt Kantor <the.matt.kantor@gmail.com>
2024-07-18 14:07:10 -0400

    Implement proverb.

--- proverb/src/lib.rs
+++ proverb/src/lib.rs
@@ -1,3 +1,17 @@
 pub fn build_proverb(list: &[&str]) -> String {
-    unimplemented!("build a proverb from this list of items: {:?}", list)
+    match list.split_first() {
+        None => String::new(),
+        Some((&head, tail)) => tail
+            .iter()
+            .scan(head, |previous, &current| {
+                let line = Some(format!(
+                    "For want of a {} the {} was lost.\n",
+                    previous, current
+                ));
+                *previous = current;
+                line
+            })
+            .chain([format!("And all for the want of a {}.", head)])
+            .collect(),
+    }
 }
